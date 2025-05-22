972fc6084fc9b25f4241c37e3ffbb4e4fdb58f52
CiiceroMenezes <cicero.lopes.menezes@gmail.com>
2025-05-22 00:09:24 +0000

    Recebendo dados Carta1

--- Aula2SuperTrunfo.c
+++ Aula2SuperTrunfo.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+struct Carta
+{ // struct da carta definido
+    char estado[50];
+    char codigo[20];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+};
 int main()
 {
+    // Desafio aula 2 - comparando duas cartas
+    struct Carta carta1, carta2;
 
+    printf("### Super Trunfo em C ###\n"); // Apresentacao de inicio de game
+    // Receber dados das cartas
+    printf("Cadastro da Primeira Carta:\n");
+    printf("Digite o Estado:\n");
+    scanf("%s", &carta1.estado);
 
-    
+    printf("Digite o codigo da carta:\n");
+    scanf("%s", &carta1.codigo);
+
+    printf("Digite o nome da Cidade:\n");
+    scanf("%s", &carta1.cidade);
+
+    printf("Digite a População:\n");
+    scanf("%d", &carta1.populacao);
+
+    printf("Digite a Area:\n");
+    scanf("%f", &carta1.area);
+
+    printf("Digite o Pib:\n");
+    scanf("%f", &carta1.pib);
+
+    printf("Digite a quantidade de Pontos Turísticos:\n");
+    scanf("%d", &carta1.pontosTuristicos);
 }
